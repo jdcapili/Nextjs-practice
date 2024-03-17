38bb4b345401d624eed06a5701721b9af1585ebe
jdcapili <jdcapili12@gmail.com>
2024-03-17 13:11:53 -0400

    Changes: - Added test pages to familiarize with Next.js routing - Added first Link components to familiarize with Pages routing.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -13,6 +14,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">src/pages/index.tsx</code>
         </p>
+        <h1>Read <Link href="/posts/first-post">this page!</Link></h1>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
