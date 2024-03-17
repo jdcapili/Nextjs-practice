3a2c01d549dfcb713becdd4b028d39c6ec9df0a1
jdcapili <jdcapili12@gmail.com>
2024-03-17 14:20:48 -0400

    Assets, Metadata, and CSS section progress

--- src/pages/posts/first-post.tsx
+++ src/pages/posts/first-post.tsx
@@ -1,8 +1,28 @@
+import Head from "next/head";
+import Image from "next/image";
 import Link from "next/link";
+// import Script from 'next/script';
+import Layout from "@/components/layout";
 
 export default function FirstPost() {
-    return <>
+    return <Layout>
+        <Head>
+            <title>First Post</title>
+        </Head>
+        {/* <Script
+            src="https://connect.facebook.net/en_US/sdk.js"
+            strategy="lazyOnload"
+            onLoad={() =>
+            console.log(`script loaded correctly, window.FB has been populated`)
+            }
+        /> */}
+        <Image
+            src="/images/sleeping-cubone.png"
+            height={144}
+            width={144}
+            alt="eepy"
+        />
         <h1>First Post</h1>
         <h2><Link href="/">Back to Home</Link></h2>
-    </>
+    </Layout>
 }
